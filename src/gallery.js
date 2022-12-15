db834d8c232e6827e80daab62037d0200748406e
RexBasiliscus <alexander-219@outlook.com>
2022-12-15 16:58:08 +0100

    created menu and gallery pages

--- src/gallery.js
+++ src/gallery.js
@@ -1,12 +1,83 @@
+import food1 from "./assets/images/food1.jpg"
+import food2 from "./assets/images/food2.jpg"
+import food3 from "./assets/images/food3.jpg"
+import food4 from "./assets/images/food4.jpg"
+import food5 from "./assets/images/food5.jpg"
+import food6 from "./assets/images/food6.jpg"
+import food7 from "./assets/images/food7.jpg"
+import food8 from "./assets/images/food8.jpg"
+import food9 from "./assets/images/food9.jpg"
+import food10 from "./assets/images/food10.jpg"
+import food11 from "./assets/images/food11.jpg"
+import food12 from "./assets/images/food12.jpg"
+import wine from "./assets/images/wine.jpg"
+import interior1 from "./assets/images/interior1.jpg"
+import interior2 from "./assets/images/interior2.jpg"
+import bar1 from "./assets/images/bar1.jpg"
 
 
 const createGallery = () => {
   const gallery = document.createElement("div");
   gallery.classList.add("gallery");
 
-  const items = document.createElement("p");
-  items.textContent = "Gallery Page";
-  gallery.appendChild(items);
+    const quote = document.createElement("p");
+    quote.textContent = `In Lucerro, every dish is made with love and care by our passionate team`;
+    gallery.appendChild(quote);
+
+  function addImg(name) {
+    const img = document.createElement("img");
+    img.src = `${name}`;
+    img.alt = `${name}`;
+    return img;
+  } 
+        
+    gallery.appendChild(
+      addImg(food1));
+        
+    gallery.appendChild(
+      addImg(food2));
+        
+    gallery.appendChild(
+      addImg(food3));
+        
+    gallery.appendChild(
+      addImg(food4));
+        
+    gallery.appendChild(
+      addImg(food5));
+        
+    gallery.appendChild(
+      addImg(food6));
+        
+    gallery.appendChild(
+      addImg(food7));
+        
+    gallery.appendChild(
+      addImg(food8));
+        
+    gallery.appendChild(
+      addImg(food9));
+        
+    gallery.appendChild(
+      addImg(food10));
+        
+    gallery.appendChild(
+      addImg(food11));
+        
+    gallery.appendChild(
+      addImg(food12));
+        
+    gallery.appendChild(
+      addImg(wine));
+        
+    gallery.appendChild(
+      addImg(interior1));
+        
+    gallery.appendChild(
+      addImg(interior2));
+        
+    gallery.appendChild(
+      addImg(bar1));
 
   return gallery;
 }
--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,101 @@
+import food1 from "./assets/images/food1.jpg"
+import food2 from "./assets/images/food2.jpg"
+import food3 from "./assets/images/food3.jpg"
+import food4 from "./assets/images/food4.jpg"
+import food5 from "./assets/images/food5.jpg"
+import food6 from "./assets/images/food6.jpg"
+import food7 from "./assets/images/food7.jpg"
+import food8 from "./assets/images/food8.jpg"
+import food9 from "./assets/images/food9.jpg"
+import food10 from "./assets/images/food10.jpg"
+import food11 from "./assets/images/food11.jpg"
+import food12 from "./assets/images/food12.jpg"
+
+
 const createMenu = () => {
   const menu = document.createElement("div");
   menu.classList.add("menu");
   
-  const items = document.createElement("p");
-  items.textContent = "Menu Page";
-  menu.appendChild(items);
+  menu.innerHTML = `
+    <h1>Our Menu</h1>
+    <div class="statement">
+      <p>The food in Lucerro is a product of teamwork in the kitchen, whether it’s the creative or the actual technical process of cooking.<br><br>
+      We pride ourselves on cooking in a natural way, using local ingredients, keeping up with the season, and making about 90% of our products in-house.
+      <br><br>
+      Sometimes we get inspiration from old traditional Slovenian recipes, sometimes we take a different approach.
+      <br><br>
+      The main objective is that we are free … so the guest can be free as well.</p>
+    </div> 
+    <div class="servings">
+      <div class="course">
+        <p>For lunch, we serve a short tasting menu:
+          <li>3 courses - 35 €</li>
+          <li>4 courses - 59 €</li>
+        </p>
+      </div>
+      <div class="course">
+        <p>For dinner, we only serve a tasting menu, which changes very often.</p>
+      </div>
+      <div class="course">
+        <p>"Carte blanche" is a way to choose dishes based on the chef’s inspiration and a selection of fresh local ingredients.
+          <li>7-course menu - 110 €, wine pairing 40 €</li>
+          <li>9-course menu - 130€, wine pairing 50 €</li>
+        </p>
+      </div>
+    </div>
+  `
+
+  const menuGallery = document.createElement("div");
+  menuGallery.classList.add("menu-gallery");
+  menu.appendChild(menuGallery);
+
+  function addImg(name) {
+    const img = document.createElement("img");
+    img.src = `${name}`;
+    img.alt = `${name}`;
+    return img;
+  } 
+        
+    menuGallery.appendChild(
+      addImg(food1));
+        
+    menuGallery.appendChild(
+      addImg(food2));
+        
+    menuGallery.appendChild(
+      addImg(food3));
+        
+    menuGallery.appendChild(
+      addImg(food4));
+        
+    menuGallery.appendChild(
+      addImg(food5));
+        
+    menuGallery.appendChild(
+      addImg(food6));
+        
+    menuGallery.appendChild(
+      addImg(food7));
+        
+    menuGallery.appendChild(
+      addImg(food8));
+        
+    menuGallery.appendChild(
+      addImg(food9));
+        
+    menuGallery.appendChild(
+      addImg(food10));
+        
+    menuGallery.appendChild(
+      addImg(food11));
+        
+    menuGallery.appendChild(
+      addImg(food12));
 
   return menu;
 }
+
+
 
 const loadMenu = () => {
   const main = document.querySelector(".main");
